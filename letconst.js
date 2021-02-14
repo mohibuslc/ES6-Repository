4cc6a93d5c0f3df5b0db97b32c3b68ee93befbe6
mohibuslc <mohib_uslc@outlook.com>
2021-02-14 22:36:47 +0000

    Discuss about Let and const Variable

--- letconst.js
+++ letconst.js
@@ -1,3 +1,8 @@
+// class Number: 22-1:
+//=================================================
+// const variable are Not Changeable ......
+
+
 const hubby = "Elias Kansun";
 
 console.log(hubby);
@@ -6,5 +11,23 @@ const numbers = [12,66];
 numbers[1] = 199;
 numbers.push(45);
 
-console.log(numbers);
+//console.log(numbers);
 
+const nayok = { name : " Kumar Shanu", PhoneNumnber: 998877};
+///console.log(nayok);
+
+
+// let variable are changeable ..
+
+let PatientName = " Rohim Caca";
+PatientName ="Fatima Begum";
+
+console.log(PatientName);
+
+let sum = 0;
+
+for (let i=0; i< 100; i++){
+   sum = sum+i;
+    console.log(i);
+
+}
