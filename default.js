e75353d1f84a1ebd2397a2e6f090ad1f6752dd0b
mohibuslc <mohib_uslc@outlook.com>
2021-02-14 22:58:55 +0000

    ES6+ style add

---
+++ default.js
@@ -0,0 +1,9 @@
+function add (num1, num2 =20 ){ // function add (num1, num2==20) its style make of ES6: if you not mention or Mistakly not put value then its take Auto
+    return num1+num2;
+
+}
+const total =add(15,);
+
+console.log(total);
+
+// 
