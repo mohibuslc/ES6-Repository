d0650712548aa8003a1e9bbb64eef49eb36b1f54
mohibuslc <mohib_uslc@outlook.com>
2021-02-16 21:00:57 +0000

    Template

---
+++ Template.js
@@ -0,0 +1,8 @@
+const firstName = " Mohammad";
+const MiddleName = " Mohibur";
+const LastName = " Rahman";
+
+const fullName = firstName+MiddleName+LastName+" Is a Good Programmer";
+const fullname2 = '${firstName} ${MiddleName}';
+
+console.log(fullName2);
---
+++ arrow.js
@@ -0,0 +1,16 @@
+// arrow function in ES6;
+
+
+// function doubleIt(num){
+
+//     return num *2;
+
+// }
+
+const doubleIt = function(num){
+
+    return num * 2;
+    
+}
+const result = doubleIt(5);
+console.log(result);
