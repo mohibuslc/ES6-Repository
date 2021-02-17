50c223fa3950b86fae0a3af594870d8c7c483fea
mohibuslc <mohib_uslc@outlook.com>
2021-02-17 10:37:26 +0000

    class

---
+++ class.js
@@ -0,0 +1,18 @@
+class Stu{
+
+    constructor(sId, Sname){
+
+        this.id=sId;
+        this.name = "Sname "
+        this.School =" Syead Hatim Ali School ";
+        
+    }
+
+}
+
+const student1 = new Stu(12, "Rahman");
+const student2 = new Stu(35, "Kamal");
+console.log(student1, student2 );
+
+
+
